065f99769aca199fda7418b025cab16d1844961c
5647kr <5647kr@gmail.com>
2025-05-12 16:08:20 +0900

    [✨ Feat] App Router로 page/Home 연결

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./style/GlobalStyle";
 import styled from "styled-components";
+import Home from "./pages/Home/Home";
+
+
 
 
 function App() {
   return (
     <Wrap>
-      <GlobalStyle /> 
+      <GlobalStyle />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
     </Wrap>
   );
 }
 export default App;
 
-const Wrap = styled.body`
-  max-width: 390px;
+const Wrap = styled.div`
+  max-width: 480px;
   height: 100vh;
   background-color: var(--bg-color);
   margin: 0 auto;
