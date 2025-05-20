4e063b1f47ea6f31ff597874dd3bbe4d5461e4b9
5647kr <5647kr@gmail.com>
2025-05-20 15:53:02 +0900

    [🖌 Design] page/Search UI 디자인_2

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,70 +1,90 @@
-import React, { useState } from 'react'
-import Header from '../../components/Header'
-import { Wrap, Title } from '../../components/Common'
-import styled from 'styled-components'
-import TypeData from './TypeData'
-import { Link } from 'react-router-dom'
-
-
-
-
-
+import React, { useState } from "react";
+import Header from "../../components/Header";
+import { Wrap, Title } from "../../components/Common";
+import styled from "styled-components";
+import TypeData from "./TypeData";
+import { Link } from "react-router-dom";
 
 export default function Search() {
   const [isDogType, setIsDogType] = useState(true);
+  const [isClicked, setIsClicked] = useState(false);
+  const [petType, setPetType] = useState("");
 
   function typeSelected(e) {
     const type = e.target.textContent;
-    console.log(type)
     type === "강아지" ? setIsDogType(true) : setIsDogType(false);
-    console.log(isDogType)
   }
-
-
 
   return (
     <>
       <Header />
       <Wrap>
-        <SearchForm>
+        <SearchWrap>
           <Title>짝궁 선택</Title>
           <strong>품종 하나만 선택해주세요.</strong>
 
           <TypeSelect>
-            <input type="radio" id='dogType' name='type' defaultChecked/>
-            <label htmlFor="dogType" onClick={(e) => {typeSelected(e)}}>강아지</label>
-            <input type="radio" id='catType' name='type'/>
-            <label htmlFor="catType" onClick={(e) => {typeSelected(e)}}>고양이</label>
+            <input type="radio" id="dogType" name="type" defaultChecked />
+            <label
+              htmlFor="dogType"
+              onClick={(e) => {
+                typeSelected(e);
+              }}
+            >
+              강아지
+            </label>
+            <input type="radio" id="catType" name="type" />
+            <label
+              htmlFor="catType"
+              onClick={(e) => {
+                typeSelected(e);
+              }}
+            >
+              고양이
+            </label>
           </TypeSelect>
 
-          <TypeData isDogType = {isDogType} />
+          <TypeData
+            isDogType={isDogType}
+            isClicked={isClicked}
+            setIsClicked={setIsClicked}
+            setPetType={setPetType}
+          />
 
-          <Link to={"/findMyPet"}>내 짝궁 찾기</Link>
-        </SearchForm>
+          {isClicked ? <Link to={"/findMyPet"} state={{petType}}>내 짝궁 찾기</Link> : <span className="disabled">내 짝궁 찾기</span> }
+        </SearchWrap>
       </Wrap>
     </>
-  )
+  );
 }
 
-
-const SearchForm = styled.form`
+const SearchWrap = styled.div`
   background-color: var(--sub-color);
   padding: 20px;
   border-radius: 20px;
   strong {
     color: var(--content-color);
   }
+  a, span {
+    width: 100%;
+    display: block;
+    padding: 10px;
+    border-radius: 20px;
+    text-align: center;
+    font-size: 1.4rem;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  }
   a {
     background-color: var(--action-color);
     color: var(--white-color);
     cursor: pointer;
-    padding: 10px;
-    border-radius: 20px;
-    font-weight: bold;
-    font-size: 1.4rem;
-    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
   }
-`
+  span {
+    border: 1px solid var(--content-color);
+    color: var(--content-color);
+    box-shadow: 0 4px 4px rgba(0,0,0,0.25);
+  }
+`;
 
 const TypeSelect = styled.div`
   margin: 20px 0 40px;
@@ -79,6 +99,7 @@ const TypeSelect = styled.div`
     border-radius: 20px;
     box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
     text-align: center;
+    cursor: pointer;
   }
   input:checked + label {
     background-color: var(--action-color);
@@ -89,5 +110,4 @@ const TypeSelect = styled.div`
   input {
     display: none;
   }
-
-`
+`;
