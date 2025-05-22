a73db8c74b64223008c41bd1ca2d11f9a2dbc28c
5647kr <5647kr@gmail.com>
2025-05-22 16:38:18 +0900

    [🖌 Design] page/Home UI 디자인_3

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,11 @@ export default function Home() {
   return (
     <>
     <Header />
-    <Wrap>
+    <HomepageWrap>
+      <IntroWrap>
+        <Title>쉽게 버려지는 유기동물</Title>
+        <p>나의 MBTI와 맞는 유기견, 유기묘를 찾아보고 <br />입양을 고려해보세요.</p>
+      </IntroWrap>
       <ContentWrap>
         <Title>나의 MBTI는?</Title>
         <p>나의 MBTI 성격 유형과 알맞는 유기묘와 유기견을 찾아<br />인생의 짝궁을 맺어보세요.</p>
@@ -21,17 +25,24 @@ export default function Home() {
       </ContentWrap>
       <ContentWrap>
         <Title>내 짝궁은?</Title>
-        <p>나와 가까운, 내가 좋아하는, 유기묘, 유기견을 찾아<br />인생의 짝궁을 맺어보세요.</p>
+        <p>MBTI를 알고 있다면, <br />바로 인생 짝궁을 맺어보세요.</p>
         <div className="imgWrap">
           <img src="/assets/img/골든_리트리버.webp" alt="골든 리트리버 아이콘" />
           <img src="/assets/img/러시안_블루.webp" alt="러시안 블루 아이콘" />
         </div>
         <Link to={"/search"}>내 짝궁 찾기</Link>
       </ContentWrap>
-    </Wrap>
+    </HomepageWrap>
     </>
   );
 }
+
+
+
+const HomepageWrap = styled(Wrap)`
+  margin-top: 0;
+  height: calc(100vh - 80px);
+`
 
 const ContentWrap = styled.div`
   background-color: var(--sub-color);
@@ -64,12 +75,31 @@ const ContentWrap = styled.div`
     font-weight: bold;
     font-size: 1.4rem;
     box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
-    display: block;
     margin: 0 auto;
     text-align: center;
-    width: fit-content;
+    width: 200px;
   }
   a:active {
     box-shadow: inset 0 4px 4px rgba(0, 0, 0, 0.25);
+  }
+`
+
+const IntroWrap = styled(ContentWrap)`
+  background: url("../../assets/img/home_bg.webp") no-repeat 50% 0 / 80%;
+  padding: 0;
+  margin: 0;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  h2 {
+    text-align: center;
+  }
+  p {
+    text-align: center;
+    font-size: 1.6rem;
+    font-weight: bold;
+    margin-bottom: 10px;
   }
 `
